bacc32d0a56ea14a52b3bf84001a820ba963ed56
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-01-15 09:54:10 -0800

    Update script.js

--- week1/script.js
+++ week1/script.js
@@ -1,1 +1,6 @@
+let firstName = "Andy";
+let lastName = "Davila";
+let timeAsAStudent = "a full quarter";
+let goal = "be a software developer";
 
+document.write(`My name is ${firstName} ${lastName}. I have been at SCC for ${timeAsAStudent} and plan to ${goal} after completing my courses. `);
