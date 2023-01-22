2311b2b4b5a6b4aec5b73b8fbaaf50233c416a10
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-01-22 15:43:48 -0800

    Update script.js

--- week1/script.js
+++ week1/script.js
@@ -2,5 +2,3 @@ let firstName = "Andy";
 let lastName = "Davila";
 let timeAsAStudent = "a full quarter";
 let goal = "be a software developer";
-
-document.write(`My name is ${firstName} ${lastName}. I have been at SCC for ${timeAsAStudent} and plan to ${goal} after completing my courses. `);
