b3ffcdcff863d3485c6f269b1d0de89d45bec99c
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-01-22 18:37:27 -0800

    Update script.js

--- Week 2/script.js
+++ Week 2/script.js
@@ -13,39 +13,31 @@
     Jan 15, 2023
 */
 
-let userInput = prompt('Pig Latin: A game of words. Please enter a word.');
-if (userInput.length > 1) {
-    switch (userInput.charAt(0)) {
-        case "a":
-        case "e":
-        case "i":
-        case "o":
-        case "u":
-        case "A":
-        case "E":
-        case "I":
-        case "O":
-        case "U":
-            document.write(userInput + "way");
-            break;
-        default:
-            switch (userInput.charAt(1)) {
-                case "a":
-                case "e":
-                case "i":
-                case "o":
-                case "u":
-                case "A":
-                case "E":
-                case "I":
-                case "O":
-                case "U":
-                    document.write(userInput.substring(1) + userInput.charAt(0) + "ay")
-                    break;
-                default:
-                    document.write(userInput.substring(2) + userInput.substring(0,2) + "ay")
-            }
-    }
-} else {
-    alert("Input cannot be translated.")
-}
+<!-- The pseudo-code, along with the author info can be found in the script.js file -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Assignment 2</title>
+    </head>
+    <body>
+        <h1>Pig Latin: a game of words.</h1>
+        <script src="script.js"></script>
+        <script>
+            let userInput = prompt('Pig Latin: A game of words. Please enter a word.');
+            if (userInput.length > 1) {
+                if (vowels.includes(userInput.charAt(0))) {
+                    document.write(userInput + "way");
+                } else {
+                    if (vowels.includes(userInput.charAt(1))) {
+                        document.write(userInput.substring(1) + userInput.charAt(0) + "ay");
+                    } else {
+                        document.write(userInput.substring(2) + userInput.substring(0,2) + "ay");
+                    }
+                }
+            } else {
+                alert("That's not a valid input.")
+            }  
+        </script>
+    </body>
+</html>
